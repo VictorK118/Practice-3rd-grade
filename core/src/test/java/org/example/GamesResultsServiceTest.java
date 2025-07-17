98888ce5e361691a73ee6bfbef36efee22e742c1
VictorK118 <vicocav.2022.18.11@gmail.com>
2025-07-18 01:45:59 +0300

    JUnit tests for GamesResultsService (rest methods)

--- core/src/test/java/org/example/GamesResultsServiceTest.java
+++ core/src/test/java/org/example/GamesResultsServiceTest.java
@@ -1,13 +1,10 @@
 package org.example;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -48,4 +45,82 @@ class GamesResultsServiceTest {
         Set<Team> result = GamesResultsService.getTopThree(fullGamesResults);
         Assertions.assertTrue(expected.containsAll(result) && result.containsAll(expected));
     }
+
+    @Test
+    public void testGetHomeTeamsWithoutAwayGoalsNull() {
+        Assertions.assertThrows(NullPointerException.class,
+                () -> GamesResultsService.getHomeTeamsWithoutAwayGoals(nullGamesResults));
+    }
+
+    @Test
+    public void testGetHomeTeamsWithoutAwayGoalsEmpty() {
+        Set<Team> result = GamesResultsService.getHomeTeamsWithoutAwayGoals(emptyGamesResults);
+        Assertions.assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void testGetHomeTeamsWithoutAwayGoalsFull() {
+        Set<Team> expected = new HashSet<>();
+        expected.add(Team.PREDATORY_BEAVERS);
+        expected.add(Team.ROOK);
+        Set<Team> result = GamesResultsService.getHomeTeamsWithoutAwayGoals(fullGamesResults);
+        Assertions.assertTrue(expected.containsAll(result) && result.containsAll(expected));
+    }
+
+    @Test
+    public void testGetTeamsWithDefeatedTeamsNull() {
+        Assertions.assertThrows(NullPointerException.class,
+                () -> GamesResultsService.getTeamsWithDefeatedTeams(nullGamesResults));
+    }
+
+    @Test
+    public void testGetTeamsWithDefeatedTeamsEmpty() {
+        Map<Team, Set<Team>> result = GamesResultsService.getTeamsWithDefeatedTeams(emptyGamesResults);
+        Assertions.assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void testGetTeamsWithDefeatedTeamsFull() {
+        Map<Team, Set<Team>> expected = Map.of(
+                Team.PREDATORY_BEAVERS, Set.of(
+                        Team.ROOK,
+                        Team.RSMU
+                ),
+                Team.ROOK, Set.of(
+                        Team.SFU,
+                        Team.RSMU
+                ),
+                Team.SFU, Set.of(
+                        Team.RSMU
+                )
+        );
+        Map<Team, Set<Team>> result = GamesResultsService.getTeamsWithDefeatedTeams(fullGamesResults);
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testGetGoalDifferenceNull() {
+        Assertions.assertThrows(NullPointerException.class,
+                () -> GamesResultsService.getGoalDifference(nullGamesResults));
+    }
+
+    @Test
+    public void testGetGoalDifferenceEmpty() {
+        Map<Team, Integer> result = GamesResultsService.getGoalDifference(emptyGamesResults);
+        Assertions.assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void testGetGoalDifferenceFull() {
+        Map<Team, Integer> result = GamesResultsService.getGoalDifference(fullGamesResults);
+        Map<Team, Integer> expected = Map.of(
+                Team.SFU, -1,
+                Team.RSMU, -6,
+                Team.PREDATORY_BEAVERS, 5,
+                Team.ROOK, 2
+        );
+        Assertions.assertEquals(expected, result);
+    }
+
+
 }
