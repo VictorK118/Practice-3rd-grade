8b2c5003bd737184da02055781ba505dfadc6bea
VictorK118 <vicocav.2022.18.11@gmail.com>
2025-07-17 20:03:30 +0300

    Initial Commit

---
+++ src/main/java/org/example/GameResult.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public record GameResult(Team homeTeam, Team awayTeam, int homeTeamGoals, int awayTeamGoals) {
+    public int getHomeTeamPoints() {
+        if (homeTeamGoals > awayTeamGoals) {
+            return 3;
+        }
+        if (homeTeamGoals == awayTeamGoals) {
+            return 1;
+        }
+        return 0;
+    }
+
+    public int getAwayTeamPoints() {
+        if (awayTeamGoals > homeTeamGoals) {
+            return 3;
+        }
+        if (awayTeamGoals == homeTeamGoals) {
+            return 1;
+        }
+        return 0;
+    }
+}
+
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,40 @@
+package org.example;
+
+/*
+В файл записываются результаты чемпионата по футболу в формате:
+Домашняя команда;Гостевая команда;счет(два числа через :)
+
+Необходимо вывести следующую информацию:
+- команды, которые заняли первые 3 места
+- команду (команды), которая не пропустила в домашнее игре ни одного мяча
+- для каждой команды, вывести список побежденных противников
+
+В задаче должны использоваться элементы функционального программирования
+Должно использоваться логгирование
+ */
+
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+    public static final String FILE_PATH = "../resources/data/file.txt";
+    private static final Logger logger = Logger.getLogger(GamesResultsFileController.class.getName());
+
+    public static void main(String[] args) throws IOException {
+        try {
+            List<GameResult> gamesResults = GamesResultsFileController.readFromFile(FILE_PATH);
+
+            System.out.println("Топ-3 команды");
+            System.out.println(GamesResultsService.getTopThree(gamesResults));
+            System.out.println("Команды, не пропустившие ни одного мяча в домашнем матче");
+            System.out.println(GamesResultsService.getHomeTeamsWithoutAwayGoals(gamesResults));
+
+            GamesResultsFileController.writeToFile(FILE_PATH, gamesResults);
+        } catch (IOException | IllegalArgumentException | NullPointerException e) {
+            logger.log(Level.SEVERE, e.toString());
+            System.out.println(e.getMessage());
+        }
+    }
+}
